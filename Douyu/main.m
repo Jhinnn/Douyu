0d9d92b7078305489bcd7c62eac4868d51a838ef
pengpeng <pengpeng@pengpengdemacbook-pro.local>
2016-01-25 20:37:30 +0800

    Initial Commit

---
+++ Douyu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Douyu
+//
+//  Created by pengpeng on 16/1/25.
+//  Copyright © 2016年 pengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
